83bfab2dc414f1a9fa285a041635efe81ad6578d
Kaarlo Sasiang <sasiang64@gmail.com>
2022-12-10 21:18:33 +0800

    Updated how script is loaded

--- script.js
+++ script.js
@@ -257,3 +257,18 @@ slider();
 //     i++;
 //   }
 // }, 3000);
+
+document.addEventListener('DOMContentLoaded', function (e) {
+  console.log('HTML parsed and DOM tree built!', e);
+});
+
+window.addEventListener('load', function (e) {
+  console.log('Page fully loaded!', e);
+});
+
+// window.addEventListener('beforeunload', function (e) {
+//   e.preventDefault();
+//   console.log(e);
+//   e.returnValue = '';
+// });
+
